d1d7470cc2407e5329fe2f8636484f1b7bad70de
gintaregud <63239813+gintaregud@users.noreply.github.com>
2020-04-28 10:49:28 +0300

    Initial commit and variable init

---
+++ js/operations.js
@@ -0,0 +1,19 @@
+"use stict";
+
+const sk1 = 27;
+const sk2 = 3;
+
+const suma = sk1 + sk2
+
+
+
+
+
+
+
+
+
+
+
+
+Console.log ( suma - skirtumas * (sandauga + sk2));
---
+++ js/variables.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*
+kintamuju:
+ - const 
+ - let
+ - var
+ */
+
+ const manoVardas = 'Rimantas';         // tekstas
+ const akiuSkaicius = 2;                // skaicius
+ const turiGalva = true;                // logine reiksme: true  false
+ const mokaSkraidyti = false            // logine reiksme: true / false
+ const petriukoPazymiai = [];                   // tuscias sarasas
+ const marytesPazymiai = [10, 8, 2];    // ne tuccias sarasas
+ const abc = ['a', 'b', 'd', 'e', 'f']; //sarasas su tekstais
+
+ console.log( manoVardas );
+ console.log( akiuSkaicius );
+ console.log( turiGalva );
+ console.log( mokaSkraidyti );
+ console.log( petriukoPazymiai );
+ console.log( marytesPazymiai );
+
+
+ let megstamasSkaicius = 5;
+ console.log( 'Megstamas skaicius:', megstamasSkaicius );
+
+ megstamasSkaicius = 3;
+ console.log( 'Megstamas skaicius:', megstamasSkaicius );
+
+ megstamasSkaicius = 7;
+ console.log( 'Megstamas skaicius:', megstamasSkaicius );
+ 
+ const belekoksSarasas = [5, 'asd', true, false, [18, '-asfdg',[]]];
+ console.log(belekoksSarasas);
+
